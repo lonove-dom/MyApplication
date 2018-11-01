a1fcf987ed8e4265eeeb815ebc612230f33fd276
lonove-dom <whh501860727@gmail.com>
2018-11-01 20:18:54 +0800

    widget1

---
+++ app/src/main/java/com/example/note/justdo/Widget/WidgetFactory.java
@@ -0,0 +1,121 @@
+package com.example.note.justdo.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.bluetooth.BluetoothClass;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.example.note.justdo.Event;
+import com.example.note.justdo.Eventdaomanger;
+import com.example.note.justdo.R;
+
+import java.util.List;
+
+public class WidgetFactory implements RemoteViewsService.RemoteViewsFactory {
+    private final static String TAG = "Widget";
+    private Context mContext;
+    private int mAppWidgetId;
+
+    private static List<Event> mEvents;
+
+    /**
+     * 构造GridRemoteViewsFactory
+     */
+    public WidgetFactory(Context context, Intent intent) {
+        mContext = context;
+        initData();
+        mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        //  HashMap<String, Object> map;
+
+        // 获取 item_widget_device.xml 对应的RemoteViews
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.item);
+
+        // 设置 第position位的“视图”的数据
+       Event event = mEvents.get(position);
+        //  rv.setImageViewResource(R.id.iv_lock, ((Integer) map.get(IMAGE_ITEM)).intValue());
+        rv.setTextViewText(R.id.textView, event.getContext());
+//        // 设置 第position位的“视图”对应的响应事件
+//        Intent fillInIntent = new Intent();
+//        fillInIntent.putExtra("Type", 0);
+//        fillInIntent.putExtra(ListWidgetProvider.COLLECTION_VIEW_EXTRA, position);
+//        rv.setOnClickFillInIntent(R.id.rl_widget_device, fillInIntent);
+//
+//
+//        Intent lockIntent = new Intent();
+//        lockIntent.putExtra(ListWidgetProvider.COLLECTION_VIEW_EXTRA, position);
+//        lockIntent.putExtra("Type", 1);
+//        rv.setOnClickFillInIntent(R.id.iv_lock, lockIntent);
+//
+//        Intent unlockIntent = new Intent();
+//        unlockIntent.putExtra("Type", 2);
+//        unlockIntent.putExtra(ListWidgetProvider.COLLECTION_VIEW_EXTRA, position);
+//        rv.setOnClickFillInIntent(R.id.iv_unlock, unlockIntent);
+        return rv;
+    }
+
+
+    /**
+     * 初始化ListView的数据
+     */
+    private void initData() {
+        mEvents=new Eventdaomanger(mContext).getfinalEventlist(1);
+    }
+
+    private static int i;
+
+//    public static void refresh() {
+//        i++;
+//        mDevices.add(new Device("Refresh" + i, 1));
+//    }
+
+    @Override
+    public void onCreate() {
+        Log.e(TAG, "onCreate");
+        // 初始化“集合视图”中的数据
+    }
+
+    @Override
+    public int getCount() {
+        // 返回“集合视图”中的数据的总数
+        return mEvents.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // 返回当前项在“集合视图”中的位置
+        return position;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        // 只有一类 ListView
+        return 1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public void onDataSetChanged() {
+    }
+
+    @Override
+    public void onDestroy() {
+        mEvents.clear();
+    }
+}
---
+++ app/src/main/java/com/example/note/justdo/Widget/WidgetProvider.java
@@ -0,0 +1,128 @@
+package com.example.note.justdo.Widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.example.note.justdo.R;
+
+public class WidgetProvider extends AppWidgetProvider {
+    public static final String CLICK_ACTION = "com.seewo.appwidgettest.action.CLICK"; // 点击事件的广播ACTION
+
+    public void updateAppWidget(Context context,AppWidgetManager appWidgetManager,int appWidgetId){
+        // 获取Widget的组件名
+        ComponentName thisWidget = new ComponentName(context,
+                WidgetProvider.class);
+        // 创建一个RemoteView
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        // 把这个Widget绑定到RemoteViewsService
+        Intent intent = new Intent(context, WidgetService.class);
+        // When intents are compared, the extras are ignored, so we need to embed the extras
+        // into the data so that the extras will not be ignored.
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        //设置适配器
+        remoteViews.setRemoteAdapter(R.id.wg_listview, intent);
+        //TODO 设置当显示的widget_list为空显示的View remoteViews.setEmptyView();
+
+        // 设置点击列表触发事件
+        Intent clickIntent = new Intent(context, WidgetProvider.class);
+        // Set the action for the intent.
+        // When the user touches a particular view, it will have the effect of
+        // broadcasting TOAST_ACTION.
+        // 设置Action，方便在onReceive中区别点击事件
+        clickIntent.setAction("clickAction");
+        clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        clickIntent.setData(Uri.parse(clickIntent.toUri(Intent.URI_INTENT_SCHEME)));
+        PendingIntent pendingIntentTemplate = PendingIntent.getBroadcast(
+                context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //使用"集合视图",如果直接setOnClickPendingIntent是不可行的,
+        //建议setPendingIntentTemplate和FillInIntent结合使用
+        //FillInIntent用于区分单个点击事件
+        remoteViews.setPendingIntentTemplate(R.id.wg_listview,
+                pendingIntentTemplate);
+
+//        // 刷新按钮
+//        final Intent refreshIntent = new Intent(context,
+//                MyRemoteAppWidget.class);
+//        refreshIntent.setAction("refresh");
+//        final PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(
+//                context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        remoteViews.setOnClickPendingIntent(R.id.button_refresh,
+//                refreshPendingIntent);
+
+
+        // 更新Widget
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        Toast.makeText(context, "用户将widget添加桌面了",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Toast.makeText(context, "用户将widget从桌面移除了",
+                Toast.LENGTH_SHORT).show();
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    /**
+     * 接受Intent
+     *
+     * @param context
+     * @param intent
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        String action = intent.getAction();
+
+        if (action.equals("refresh")) {
+            int i = 0;
+            // 刷新Widget
+            final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+            final ComponentName cn = new ComponentName(context,
+                    WidgetProvider.class);
+
+          //  MyRemoteViewsFactory.mList.add("音乐" + i);
+           // i=i+1;
+            // 这句话会调用RemoteViewSerivce中RemoteViewsFactory的onDataSetChanged()方法。
+            mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn),
+                    R.id.wg_listview);
+
+        } else if (action.equals("clickAction")) {
+            // 单击Wdiget中ListView的某一项会显示一个Toast提示。
+            Toast.makeText(context, intent.getStringExtra("content"),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
+
